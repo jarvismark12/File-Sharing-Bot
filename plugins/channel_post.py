caf4a8736cd7259b07478a82b9537074f5af3582
jarvismark12 <162993629+jarvismark12@users.noreply.github.com>
2024-08-01 19:19:24 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -20,7 +20,6 @@ async def channel_post(client: Client, message: Message):
         await reply_text.edit_text("Something went Wrong..!")
         return
 
-    # Generate the unique string and encode it to base64
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
@@ -38,8 +37,8 @@ async def channel_post(client: Client, message: Message):
         except FloodWait as e:
             await asyncio.sleep(e.value)
             await post_message.edit_caption(caption=watermark_caption, reply_markup=reply_markup)
-        except Exception:
-            pass
+        except Exception as e:
+            print(e)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
@@ -47,13 +46,12 @@ async def new_post(client: Client, message: Message):
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    # Generate the unique string and encode it to base64
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    watermark_caption = f"{message.caption or ''}\n\n(©)Codexbotz\n{base64_string}"
+    watermark_caption = f"{message.caption or ''}\n\n(©)Id\n{base64_string}"
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     try:
@@ -61,8 +59,8 @@ async def new_post(client: Client, message: Message):
     except FloodWait as e:
         await asyncio.sleep(e.value)
         await message.edit_caption(caption=watermark_caption, reply_markup=reply_markup)
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
 
 # Helper function for encoding
 import base64
